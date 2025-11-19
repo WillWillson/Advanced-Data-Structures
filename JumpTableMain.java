e283528533558d6ffcf0d4399c1819a4636a6a58
WilliamWilson <108369133+WillWillson@users.noreply.github.com>
2025-11-18 22:53:09 -0600

    Delete JumpTableMain.java

--- JumpTableMain.java
+++
@@ -1,9 +0,0 @@
- public class JumpTableMain {
-    public static void main(String[] args) {
-        Screen screen = new Screen();
-        boolean keepRunning = true;
-        while(keepRunning) {
-            keepRunning = screen.doState();
-        }
-    }
-}
